bf1d485cbce455b8868975507181d7e344f3bbdd
Marc Marina <marc.marina.miravitlles@gmail.com>
2024-12-10 20:21:58 +0100

    Adding pictures

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { Layout } from "./layout";
 
 function App() {
   return (
-    <Layout>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-7 p-3">
-        {Array(16)
+    <Layout className="">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
+        {Array(4)
           .fill(0)
           .map(() => {
             return <ImageCard />;
@@ -18,13 +18,16 @@ export default App;
 
 const ImageCard = () => {
   return (
-    <div className="bg-white flex flex-col gap-2 p-3 shadow-md hover:shadow-xl transition-all duration-300">
+    <div className="bg-white flex flex-col gap-3 p-3 rounded">
       <img className="" src="https://picsum.photos/600/400" />
       <p className="text-lg font-semibold">This is an image description</p>
-      <p>
+      <p className="text-sm font-light">
         Nostrud occaecat laborum voluptate deserunt incididunt eiusmod enim in
         tempor anim do cillum minim.
       </p>
+      <button className="rounded border-blue-400 hover:bg-blue-100 border transition-all duration-150 w-fit m-auto text-blue-600 px-3 py-1 text-lg">
+        Submit
+      </button>
     </div>
   );
 };
--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,10 @@
+import { twMerge } from "tailwind-merge";
+
 export const Layout: React.FC<
   React.PropsWithChildren<{ className?: string }>
 > = ({ children, className }) => {
   return (
-    <div className={`w-full max-w-screen-lg h-full ${className}`}>
+    <div className={twMerge("w-full max-w-screen-lg h-full", className)}>
       {children}
     </div>
   );
