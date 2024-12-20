255784799b43e37e7df816f08e623ce3dca0279e
Marc Marina <mmarina@findmypast.com>
2024-12-20 14:50:49 +0100

    Added prettier-tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Layout } from "./layout";
 function App() {
   return (
     <Layout className="">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 p-3">
+      <div className="grid grid-cols-1 gap-3 p-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {Array(4)
           .fill(0)
           .map(() => {
@@ -18,14 +18,14 @@ export default App;
 
 const ImageCard = () => {
   return (
-    <div className="bg-white flex flex-col gap-3 p-3 rounded">
+    <div className="flex flex-col gap-3 rounded bg-white p-3">
       <img className="" src="https://picsum.photos/600/400" />
       <p className="text-lg font-semibold">This is an image description</p>
       <p className="text-sm font-light">
         Nostrud occaecat laborum voluptate deserunt incididunt eiusmod enim in
         tempor anim do cillum minim.
       </p>
-      <button className="rounded border-blue-400 hover:bg-blue-100 border transition-all duration-150 w-fit m-auto text-blue-600 px-3 py-1 text-lg">
+      <button className="m-auto w-fit rounded border border-blue-400 px-3 py-1 text-lg text-blue-600 transition-all duration-150 hover:bg-blue-100">
         Submit
       </button>
     </div>
