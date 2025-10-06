135ea92037cb60229021105eb42f7357b0d73ffa
Marc Marina <mmarina@findmypast.com>
2025-10-06 21:47:34 +0200

    Added react query and axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
 import { Layout } from "./layout";
+import { client } from "./client";
 
 function App() {
+  const usersQuery = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await client.get<User[]>("/users");
+
+      return res.data;
+    },
+  });
+
+  if (usersQuery.isLoading) {
+    return <h1>Loading</h1>;
+  }
+
   return (
     <Layout className="">
       <div className="grid grid-cols-1 gap-3 p-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {Array(4)
-          .fill(0)
-          .map(() => {
-            return <ImageCard />;
-          })}
+        {usersQuery.data!.map((u) => {
+          return <ImageCard user={u} />;
+        })}
       </div>
     </Layout>
   );
@@ -16,18 +29,23 @@ function App() {
 
 export default App;
 
-const ImageCard = () => {
+const ImageCard = ({ user }: { user: User }) => {
   return (
     <div className="flex flex-col gap-3 rounded bg-white p-3">
       <img className="" src="https://picsum.photos/600/400" />
-      <p className="text-lg font-semibold">This is an image description</p>
+      <p className="text-lg font-semibold">{user.id}</p>
       <p className="text-sm font-light">
-        Nostrud occaecat laborum voluptate deserunt incididunt eiusmod enim in
-        tempor anim do cillum minim.
+        {user.firstName} {user.lastName}
       </p>
       <button className="m-auto w-fit rounded border border-blue-400 px-3 py-1 text-lg text-blue-600 transition-all duration-150 hover:bg-blue-100">
         Submit
       </button>
     </div>
   );
+};
+
+type User = {
+  id: number;
+  firstName: string;
+  lastName: string;
 };
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div className="bg-[#f3f0ec] min-h-dvh flex justify-center">
-      <App />
+    <div className="flex min-h-dvh justify-center bg-[#f3f0ec]">
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools position="right" />
+        <App />
+      </QueryClientProvider>
     </div>
-  </StrictMode>
+  </StrictMode>,
 );
